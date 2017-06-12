7ca5859f1f040cc4d5caf93e768ee1da9043e800
Aidana <at33859n@pace.edu>
2017-06-12 14:03:30 +0600

    Initial Commit

---
+++ Tweet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweet
+//
+//  Created by Айдана Токбаева on 6/12/17.
+//  Copyright © 2017 Aidana Tokbayeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
